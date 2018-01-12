80ec7805b0c0fc4f372de66c7d41c8c62bb4b971
Evgeniy Morskoy <zhekamorskoi@gmail.com>
2018-01-12 21:45:16 +0200

    Add GET/ todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -14,7 +15,6 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
 
-
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -28,6 +28,27 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+// app.get('/todos/:id', (req, res) => {
+//   res.send(req.params);
+// });
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
