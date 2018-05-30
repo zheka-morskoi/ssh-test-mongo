ddc268ee262a7f3c13d184ac304a07d0d4ac8b7a
Evgeniy Morskoy <zhekamorskoi@gmail.com>
2018-05-30 22:01:33 +0300

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -16,25 +16,19 @@ app.post("/todos", (req, res) => {
     text: req.body.text
   });
 
-  todo.save().then(
-    doc => {
+  todo.save().then((doc) => {
       res.send(doc);
-    },
-    e => {
+    }, (e) => {
       res.status(400).send(e);
-    }
-  );
+    });
 });
 
 app.get("/todos", (req, res) => {
-  Todo.find().then(
-    todos => {
+  Todo.find().then((todos) => {
       res.send({ todos });
-    },
-    e => {
+    },(e) => {
       res.status(400).send(e);
-    }
-  );
+    });
 });
 
 app.get("/todos/:id", (req, res) => {
@@ -44,15 +38,13 @@ app.get("/todos/:id", (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findById(id)
-    .then(todo => {
+  Todo.findById(id).then((todo) => {
       if (!todo) {
         return res.status(404).send();
       }
 
       res.send({ todo });
-    })
-    .catch(e => {
+    }).catch((e) => {
       res.status(400).send();
     });
 });
@@ -64,15 +56,13 @@ app.delete("/todos/:id", (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id)
-    .then(todo => {
+  Todo.findByIdAndRemove(id).then((todo) => {
       if (!todo) {
         return res.status(404).send();
       }
 
-      res.send(todo);
-    })
-    .catch(e => {
+      res.send({ todo });
+    }).catch((e) => {
       res.status(400).send();
     });
 });
